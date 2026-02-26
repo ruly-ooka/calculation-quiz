7c1b81303464bcb41d42a9a76f653c14f5053864
ruly-ooka <ayana.ruly@outlook.jp>
2026-02-26 10:20:50 +0900

    初期コミット

---
+++ src/main/java/jp/ruly/training/ooka/calculation_quiz/CalculationQuizApplication.java
@@ -0,0 +1,13 @@
+package jp.ruly.training.ooka.calculation_quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculationQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/ruly/training/ooka/calculation_quiz/CalculationQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ruly.training.ooka.calculation_quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculationQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
