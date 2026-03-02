0e006c86fe4dc9af9f217c534cbe15f7af96a129
ruly-ooka <ayana.ruly@outlook.jp>
2026-03-02 15:18:30 +0900

    初期表示用コントローラーメソッドを追加

--- src/main/java/jp/ruly/training/ooka/calculation_quiz/contoller/GenerateQuizController.java
+++ src/main/java/jp/ruly/training/ooka/calculation_quiz/contoller/GenerateQuizController.java
@@ -1,12 +1,17 @@
 package jp.ruly.training.ooka.calculation_quiz.contoller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GenerateQuizController {
-	
+	@GetMapping("/init")
+	public String init() {
+		return "index.html";
+	}
+
 	@PostMapping("/generate-questions")
 	public String generate(@RequestParam("operator") int operator, @RequestParam("count") int count) {
 		return "index.html";
